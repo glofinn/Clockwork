ade433dbbaafcb112bf0bee85a87f034c615d14f
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-01-10 18:06:55 +0100

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavasScript";
+document.querySelector("body").appendChild(h2);
+
+const p1 = document.createElement("h2");
+p1.textContent = "Little paragraph of nonsense";
+document.querySelector("body").appendChild(p1);
